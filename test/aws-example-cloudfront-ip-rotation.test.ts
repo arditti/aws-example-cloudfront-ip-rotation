88176f4d20ead696db441091b210ff28fc32017d
Eitav Arditti <eitvrdt@gmail.com>
2025-05-04 09:40:39 +0300

    Initial commit

---
+++ test/aws-example-cloudfront-ip-rotation.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsExampleCloudfrontIpRotation from '../lib/aws-example-cloudfront-ip-rotation-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-example-cloudfront-ip-rotation-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsExampleCloudfrontIpRotation.AwsExampleCloudfrontIpRotationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
